0a29438f3d802786c61ac5e9338bd7c9305e6886
Jenkins Automation Server <jenkins@ip-172-31-4-99.ap-south-1.compute.internal>
2026-09-11 05:49:21 +0000

    Initial Maven project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins + Maven!");
+        System.out.println("Java 21 Maven project is working.");
+    }
+}
